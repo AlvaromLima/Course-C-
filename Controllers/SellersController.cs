af63847588afb1a32c438d1f9416f06d0d44b899
Alvaro Lima <alvaromlima@gmail.com>
2020-11-03 16:19:37 -0300

    Seller details and eager loading

--- Controllers/SellersController.cs
+++ Controllers/SellersController.cs
@@ -63,5 +63,30 @@ namespace SalesWebMvc.Controllers
             return RedirectToAction(nameof(Index));
         }
 
+        public IActionResult Details(int? id)
+        {
+
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var obj = _sellerService.FindById(id.Value);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Details(int id)
+        {
+            _sellerService.Remove(id);
+            return RedirectToAction(nameof(Index));
+        }
+
     }
 }
